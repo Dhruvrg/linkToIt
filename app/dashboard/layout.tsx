301f6f5ceddacad44a010a327cd3a86c5291a0fb
Dhruv <dhruvrg2003@gmail.com>
2024-12-31 03:19:47 +0530

    added loader

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,12 +1,15 @@
 "use client";
 
 import ClientOnly from "@/components/ClientOnly";
+import Loader from "@/components/Loader";
 import CreateProjectModal from "@/components/modals/CreateProjectModal";
 import Navbar from "@/components/navbar/Navbar";
 import SideBar from "@/components/SideBar";
 import getCurrentUser from "@/lib/actions/getCurrentUser";
 import { getProjects } from "@/lib/actions/project.actions";
-import { redirect, useRouter } from "next/navigation";
+import { SafeUser } from "@/types";
+import { Project } from "@prisma/client";
+import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 
 export default function RootLayout({
@@ -14,14 +17,15 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const [currentUser, setCurrentUser] = useState<any>(null);
-  const [projects, setProjects] = useState([]);
+  const [currentUser, setCurrentUser] = useState<SafeUser>();
+  const [projects, setProjects] = useState<Project[]>();
   const router = useRouter();
 
   useEffect(() => {
     async function fetchUser() {
       const user = await getCurrentUser();
       const projects = await getProjects();
+      setProjects(projects);
 
       if (!user) {
         router.push("/");
@@ -37,7 +41,7 @@ export default function RootLayout({
   }, []);
 
   if (!currentUser) {
-    // Optionally render a loader here
+    <Loader />;
     return null;
   }
 
